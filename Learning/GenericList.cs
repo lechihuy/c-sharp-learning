11de28cb2c2857690c5df6f1c4f90247485b690b
Lê Chí Huy <lechihuy1062001@gmail.com>
2022-06-20 22:35:51 +0700

    generic types

---
+++ Learning/GenericList.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace Learning;
+
+public class GenericList<T>
+{
+    private Node? head;
+
+    public GenericList()
+    {
+        head = null;
+    }
+
+    private	class Node
+    {
+        private Node? next;
+
+        private T data;
+
+        public Node(T t)
+        {
+            next = null;
+            data = t;
+        }
+
+        public Node? Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+    }
+
+    public void AddHead(T t)
+    {
+        Node n = new Node(t);
+        n.Next = head;
+        head = n;
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        Node? current = head;
+
+        while (current != null)
+        {
+            yield return current.Data;
+            current = current.Next;
+        }
+    }
+}
+
--- Learning/Program.cs
+++ Learning/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿using Learning;
+
+GenericList<int> list = new GenericList<int>();
+
+for (int x = 0; x < 10; x++)
+{
+    list.AddHead(x);
+}
+
+foreach (int i in list)
+{
+    System.Console.Write(i + " ");
+}
+System.Console.WriteLine("\nDone");
