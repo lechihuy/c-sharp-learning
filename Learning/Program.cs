835550bb729bd64e86ffd5b9631c3d3d56103b02
Lê Chí Huy <lechihuy1062001@gmail.com>
2022-06-21 12:13:30 +0700

    swap two variables

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -4,5 +4,12 @@ int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Enter the second number:");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Total:");
-Console.WriteLine(firstNumber + secondNumber);
+int temp;
+
+temp = firstNumber;
+firstNumber = secondNumber;
+secondNumber = temp;
+
+Console.WriteLine("Swap two number:");
+Console.WriteLine($"First number:" + firstNumber);
+Console.WriteLine($"Second number:" + secondNumber);
