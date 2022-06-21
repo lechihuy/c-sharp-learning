0ef3ae3fba0b16944eb4ba682a947607d4f04287
Lê Chí Huy <lechihuy1062001@gmail.com>
2022-06-21 12:07:02 +0700

    two number addition

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -1,1 +1,8 @@
-﻿Console.WriteLine("Hello world");
+﻿Console.WriteLine("Enter the first number:");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the second number:");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Total:");
+Console.WriteLine(firstNumber + secondNumber);
