2c388fbe8e798bb8458e6e4c53874390c3be4cac
Lê Chí Huy <lechihuy1062001@gmail.com>
2022-06-21 12:01:28 +0700

    hello world

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -1,14 +1,1 @@
-﻿using Learning;
-
-GenericList<int> list = new GenericList<int>();
-
-for (int x = 0; x < 10; x++)
-{
-    list.AddHead(x);
-}
-
-foreach (int i in list)
-{
-    System.Console.Write(i + " ");
-}
-System.Console.WriteLine("\nDone");
+﻿Console.WriteLine("Hello world");
